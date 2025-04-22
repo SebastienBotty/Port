9ff8169af5cf2f85fa79da1dbad2f1ae77fdb03f
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-04-22 23:32:26 +0200

    responsive homepage

--- src/Components/Flag.tsx
+++ src/Components/Flag.tsx
@@ -1,11 +1,6 @@
 import { LanguageType } from "../Typescript/Types";
+import "../scss/flag.scss";
 
 export const Flag = ({ countryCode }: { countryCode: LanguageType | "BE" }) => {
-  return (
-    <img
-      src={`/flags/${countryCode}-flag.svg`}
-      alt={countryCode}
-      style={{ width: "24px", height: "16px", verticalAlign: "middle" }}
-    />
-  );
+  return <img src={`/flags/${countryCode}-flag.svg`} alt={countryCode} className="flag" />;
 };
