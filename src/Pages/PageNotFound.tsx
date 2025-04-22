43a11d1ddad3ee042456bdb2bcc20ad6103f785a
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-04-22 20:18:04 +0200

    PageNotFound translate msg

--- src/Pages/PageNotFound.tsx
+++ src/Pages/PageNotFound.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../scss/pageNotFound.scss"; // Assurez-vous d'importer le fichier CSS
 import { Link } from "react-router-dom";
+import { useLanguageContext } from "../Contexts/useLanguage";
 
 function PageNotFound() {
+  const { language } = useLanguageContext();
+  const msg = {
+    error: {
+      FR: "La page que vous recherchez n'existe pas...",
+      EN: "Oops, this page doesn't exist...",
+    },
+    goBack: {
+      FR: "Retoue à la page d'accueil",
+      EN: "Back to home page",
+    },
+  };
   return (
     <div className="page-not-found">
       <h1>404</h1>
-      <p>La page que vous recherchez n'existe pas.</p>
+      <p>{msg.error[language]}</p>
       <Link to="/" className="home-link">
-        Retour à la page d'accueil
+        {msg.goBack[language]}
       </Link>
     </div>
   );
